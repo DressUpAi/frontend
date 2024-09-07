7947f0fe3997da7d8c998f5be3c96afc4f5a46b8
sanjay-29-29 <sanjaysusi29@gmail.com>
2024-09-07 14:11:32 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <>
+      <div className="grid grid-cols-2 h-screen w-screen gap-2">
+        <div className="grid grid-rows-2 gap-2">
+          <div className="grid-cols-2 border-2 border-dashed m-2">
+             
+          </div>
+          <div className="border-2 border-dashed mt-2">
+          </div>
+        </div>
+        <div className="border-2 border-dashed">
+        </div>
+      </div> 
+    </>
+   )
+}
