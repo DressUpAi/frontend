1cf0dc3c8a95c7111bc1011f8645121078a5df9a
sanjay-29-29 <sanjaysusi29@gmail.com>
2024-09-07 15:31:45 +0530

    added grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 export default function App() {
   return (
     <>
-      <div className="grid grid-cols-2 h-screen w-screen gap-2">
+      <div className="grid grid-cols-2 h-screen w-screen gap-2 p-2">
         <div className="grid grid-rows-2 gap-2">
-          <div className="grid-cols-2 border-2 border-dashed m-2">
-             
-          </div>
-          <div className="border-2 border-dashed mt-2">
-          </div>
+          <div className="grid grid-cols-2 gap-2 p-2">
+            <div className="h-full border-2 border-dashed rounded-lg flex flex-row items-center justify-center">
+              <FontAwesomeIcon icon={faPlus} />
+            </div>
+            <div className="border">
+            </div>
+          </div> 
+          <div className="grid grid-cols-2 gap-2 p-2"> 
+            <div className="h-full border-2 border-dashed rounded-lg flex flex-row items-center justify-center">
+              <FontAwesomeIcon icon={faPlus} />
+            </div>
+            <div className="border">
+            </div>
+          </div> 
+        </div> 
+        <div className="border-2">
         </div>
-        <div className="border-2 border-dashed">
-        </div>
+      
       </div> 
     </>
    )
